9f54ec822e13342a0f13d9342ab1566ccbf8e592
TaeWooKim-SCH <zop1234@hanmail.net>
2023-11-16 14:06:32 +0900

    feat: 캘린더 css 커스텀

--- src/app/_components/CalendarContainer.tsx
+++ src/app/_components/CalendarContainer.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import Calendar from "react-calendar";
 
-import 'react-calendar/dist/Calendar.css';
+import '../_styles/CalendarContainer.css';
 
 export default function CalendarContainer() {
   const [value, setValue] = useState(new Date());
