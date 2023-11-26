440bd6e8972aaf36d556785cafdb074085f1158e
TaeWooKim-SCH <zop1234@hanmail.net>
2023-11-26 14:53:50 +0900

    feat: 예약 거절 후 예약 불가 컬렉션 반영

--- src/app/api/admin/approve/route.ts
+++ src/app/api/admin/approve/route.ts
@@ -4,11 +4,26 @@ import { Db, ObjectId } from "mongodb";
 export async function POST(request: Request) {
   const body = await request.json();
   const db: Db = await connectDB();
-  const updateReserv = await db.collection('reservation-list').updateOne({
+  const getReserv = await db.collection("reservation-list").findOne(new ObjectId(body._id));
+  const updateReserv = await db.collection("reservation-list").updateOne({
     _id: new ObjectId(body._id)
   }, {
     $set: {reservationState: body.reservationState}
   });
+  console.log(body);
+
+  if (body.reservationState === "거절" && getReserv) {
+    const timeObj: {[key: string]: boolean} = {};
+    for (let time of getReserv.time) {
+      timeObj[time] = false;
+    }
+    const updateNotPossible = await db.collection("not-possible-list").updateOne({
+      date: getReserv.date,
+      roomNumber: getReserv.roomNumber
+    }, {
+      $set: timeObj
+    })
+  }
 
   if (updateReserv.acknowledged) {
     return Response.json("성공", { status: 200 });
